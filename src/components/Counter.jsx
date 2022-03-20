45f63bbb318125477159ccb8df268a5e67c5cdc3
Sajidur Rahman <sajibsrs@gmail.com>
2022-03-20 15:53:22 +0600

    format, added-context provider

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,26 +1,33 @@
-import { useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import ReceiveContext from "./ReceiveContext";
+
+export const ThemeContext = createContext();
 
 function CountDown() {
-    const [counter, setCounter] = useState(10);
+  const [counter, setCounter] = useState(10);
 
-    useEffect(() => {
-        let timer;
+  useEffect(() => {
+    let timer;
 
-        if (counter > 0) {
-            timer = setTimeout(() => {
-                setCounter( counter => counter - 1);
-            }, 1000);
-        }
+    if (counter > 0) {
+      timer = setTimeout(() => {
+        setCounter(counter => counter - 1);
+      }, 1000);
+    }
 
-        return () => clearTimeout(timer);
-    }, [counter]);
+    return () => clearTimeout(timer);
+  }, [counter]);
 
-    return (
-        <>
-            <p># useState and useEffect hooks in action</p>
-            <h2>Ready {counter}</h2>
-        </>
-    );
+  return (
+    <>
+      
+      <ThemeContext.Provider value={counter}>
+        <p># useState and useEffect hooks in action</p>
+        <h2>Ready {counter}</h2>
+        <ReceiveContext />
+      </ThemeContext.Provider>
+    </>
+  );
 }
 
 export default CountDown;
